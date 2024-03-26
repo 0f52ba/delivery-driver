fa9cf5972b68c4065bd8b16d362bc4cc8856c640
Alex <alyx8933@gmail.com>
2024-03-26 20:23:09 +0100

    delivery driver game

---
+++ Assets/Scenes/Delivery.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+   [SerializeField] Color32 hasPackageColor = new Color32(1,1,1,1);
+   [SerializeField] Color32 noPackageColor = new Color32(1,1,1,1);
+
+   [SerializeField] float destroyDelay = 0.1f;
+   bool hasPackage;
+
+   SpriteRenderer spriteRenderer;
+
+   void Start()
+   {
+      spriteRenderer = GetComponent<SpriteRenderer>();  
+   }
+
+   void OnTriggerEnter2D(Collider2D other) 
+   {
+     if(other.tag == "Package" && !hasPackage)
+     {
+        Debug.Log("Package picked up!");
+        hasPackage = true;
+        spriteRenderer.color = hasPackageColor;
+        Destroy(other.gameObject, destroyDelay);
+     }
+
+     if(other.tag == "Customer" && hasPackage)
+     {
+          Debug.Log("Delivered package.");
+          hasPackage = false;
+          spriteRenderer.color = noPackageColor;
+     }     
+   }
+}
